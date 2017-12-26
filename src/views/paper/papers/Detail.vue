efa115cb1b4137a24bed007afce203bdf7bac200
hytcwangfang <18014746239@163.com>
2017-12-26 16:44:34 +0800

    detail-problem

--- src/views/paper/papers/Detail.vue
+++ src/views/paper/papers/Detail.vue
@@ -17,35 +17,169 @@
 						<span>单选题（共1题 &nbsp; 每题2分）</span>
 						<el-button type="primary" @click.stop="addRadioQuestion">添加试题</el-button>
 					</template>
-					<div>oooooooooooo</div>
+					<div class="el-question">
+						<div class="el-question-title">
+							<span>1. 某物体在一足够大的光滑平面上向西运动，当它受到一个向南的恒定外力作用时，物体运动将是（）</span>
+						</div>
+						<div class="el-question-options">
+							<el-radio-group v-model="radioAnswer">
+							<el-radio label="A">A. 直线运动且是匀变速直线运动</el-radio>
+							<el-radio label="B">B. 曲线运动但加速度方向不变、大小不变，是匀变速运动</el-radio>
+							<el-radio label="C">C. 曲线运动但加速度方向改变、大小不变，是非匀变速曲线运动</el-radio>
+							<el-radio label="D">D. 曲线运动，加速度大小和方向均改变，是非匀变速曲线运动</el-radio>
+						</el-radio-group>
+						</div>
+						<div class="el-question-info">
+							<div class="el-info">
+								<div class="el-label el-label-light-green">正确答案</div>
+								<div class="el-info-content">A</div>
+							</div>
+							<div class="el-info">
+								<div class="el-label el-label-blue">解析</div>
+								<div class="el-info-content">光滑、摩擦力</div>
+							</div>
+							<div class="el-info">
+								<div class="el-label el-label-green">考点</div>
+								<div class="el-info-content">曲线运动，匀速运动</div>
+							</div>
+							<div class="el-info">
+								<div class="el-label el-label-purple">添加人</div>
+								<div class="el-info-content">Admin</div>
+							</div>
+						</div>
+						<div class="pageArea">
+							<Page></Page>
+						</div>
+					</div>
 				</el-collapse-item>
 				<el-collapse-item>
 					<template slot="title">
 						<span>多选题（共1题 &nbsp; 每题2分）</span>
-						<el-button type="primary" @click.stop="addRadioQuestion">添加试题</el-button>
+						<el-button type="primary" @click.stop="addCheckQuestion">添加试题</el-button>
 					</template>
-					<div>oooooooooooo</div>
+					<div class="el-question">
+						<div class="el-question-title">
+							<span>1. 某物体在一足够大的光滑平面上向西运动，当它受到一个向南的恒定外力作用时，物体运动将是（）</span>
+						</div>
+						<div class="el-question-options">
+							<el-checkbox-group v-model="checkAnswer">
+							<el-checkbox label="A">A. 直线运动且是匀变速直线运动</el-checkbox>
+							<el-checkbox label="B">B. 曲线运动但加速度方向不变、大小不变，是匀变速运动</el-checkbox>
+							<el-checkbox label="C">C. 曲线运动但加速度方向改变、大小不变，是非匀变速曲线运动</el-checkbox>
+							<el-checkbox label="D">D. 曲线运动，加速度大小和方向均改变，是非匀变速曲线运动</el-checkbox>
+						</el-checkbox-group>
+						</div>
+						<div class="el-question-info">
+							<div class="el-info">
+								<div class="el-label el-label-light-green">正确答案</div>
+								<div class="el-info-content">A,B</div>
+							</div>
+							<div class="el-info">
+								<div class="el-label el-label-blue">解析</div>
+								<div class="el-info-content">光滑、摩擦力</div>
+							</div>
+							<div class="el-info">
+								<div class="el-label el-label-green">考点</div>
+								<div class="el-info-content">曲线运动，匀速运动</div>
+							</div>
+							<div class="el-info">
+								<div class="el-label el-label-purple">添加人</div>
+								<div class="el-info-content">Admin</div>
+							</div>
+						</div>
+						<div class="pageArea">
+							<Page></Page>
+						</div>
+					</div>
 				</el-collapse-item>
 				<el-collapse-item>
 					<template slot="title">
 						<span>判断题（共1题 &nbsp; 每题2分）</span>
-						<el-button type="primary" @click.stop="addRadioQuestion">添加试题</el-button>
+						<el-button type="primary" @click.stop="addJudgeQuestion">添加试题</el-button>
 					</template>
-					<div>oooooooooooo</div>
+					<div class="el-question">
+						<div class="el-question-title">
+							<span>1. 某物体在一足够大的光滑平面上向西运动，当它受到一个向南的恒定外力作用时，物体运动将是直线运动且是匀变速直线运动。</span>
+						</div>
+						<div class="el-question-options">
+							<el-radio-group v-model="judgeAnswer">
+							<el-radio label="A">A. 正确</el-radio>
+							<el-radio label="B">B. 错误</el-radio>
+						</el-radio-group>
+						</div>
+						<div class="el-question-info">
+							<div class="el-info">
+								<div class="el-label el-label-light-green">正确答案</div>
+								<div class="el-info-content">A</div>
+							</div>
+							<div class="el-info">
+								<div class="el-label el-label-blue">解析</div>
+								<div class="el-info-content">光滑、摩擦力</div>
+							</div>
+							<div class="el-info">
+								<div class="el-label el-label-green">考点</div>
+								<div class="el-info-content">曲线运动，匀速运动</div>
+							</div>
+							<div class="el-info">
+								<div class="el-label el-label-purple">添加人</div>
+								<div class="el-info-content">Admin</div>
+							</div>
+						</div>
+						<div class="pageArea">
+							<Page></Page>
+						</div>
+					</div>
 				</el-collapse-item>
 				<el-collapse-item>
 					<template slot="title">
 						<span>选做题（共1题 &nbsp; 每题2分）</span>
-						<el-button type="primary" @click.stop="addRadioQuestion">添加试题</el-button>
+						<el-button type="primary" @click.stop="addOptionQuestion">添加试题</el-button>
 					</template>
-					<div>oooooooooooo</div>
+					<div class="el-question">
+						<div class="el-question-title">
+							<span>1. 某物体在一足够大的光滑平面上向西运动，当它受到一个向南的恒定外力作用时，物体运动将是（）</span>
+						</div>
+						<div class="el-question-options">
+							<el-radio-group v-model="optionAnswer">
+							<el-radio label="A">A. 直线运动且是匀变速直线运动</el-radio>
+							<el-radio label="B">B. 曲线运动但加速度方向不变、大小不变，是匀变速运动</el-radio>
+							<el-radio label="C">C. 曲线运动但加速度方向改变、大小不变，是非匀变速曲线运动</el-radio>
+							<el-radio label="D">D. 曲线运动，加速度大小和方向均改变，是非匀变速曲线运动</el-radio>
+						</el-radio-group>
+						</div>
+						<div class="el-question-info">
+							<div class="el-info">
+								<div class="el-label el-label-light-green">正确答案</div>
+								<div class="el-info-content">A</div>
+							</div>
+							<div class="el-info">
+								<div class="el-label el-label-blue">解析</div>
+								<div class="el-info-content">光滑、摩擦力</div>
+							</div>
+							<div class="el-info">
+								<div class="el-label el-label-green">考点</div>
+								<div class="el-info-content">曲线运动，匀速运动</div>
+							</div>
+							<div class="el-info">
+								<div class="el-label el-label-purple">添加人</div>
+								<div class="el-info-content">Admin</div>
+							</div>
+						</div>
+						<div class="pageArea">
+							<Page></Page>
+						</div>
+					</div>
 				</el-collapse-item>
 			</el-collapse>
 		</div>
 	</div>
 </template>
 <script>
+	import Page from '../../common/Pagination.vue'
 	export default{
+		components: {
+			Page
+		},
 		data(){
 			return {
 				paper:{
@@ -53,7 +187,10 @@
 					total: '100',
 					hasSet: '6'
 				},
-
+				radioAnswer:'A',
+				checkAnswer:'A,B',
+				judgeAnswer:'A',
+				optionAnswer:'A'
 			}
 		},
 		methods:{
@@ -65,6 +202,15 @@
 			},
 			addRadioQuestion(){
 				console.log('addRadioQuestion');
+			},
+			addCheckQuestion(){
+				console.log('addCheckQuestion');
+			},
+			addJudgeQuestion(){
+				console.log('addJudgeQuestion');
+			},
+			addOptionQuestion(){
+				console.log('addOptionQuestion');
 			}
 		},
 		mounted(){
@@ -74,5 +220,28 @@
 </script>
 <style lang="scss" scoped>
 	@import '~scss_vars';
-	
+	.pageArea{
+		float: right;
+	}
+	.el-question{
+		.el-question-title{
+			font-size: 16px;
+			color: #000000;
+			letter-spacing: 1px;
+			line-height: 22px;
+		}
+		.el-question-options{
+			padding: 10px;
+			.el-radio, .el-checkbox{
+				display: block;
+				margin: 20px 0;
+			}
+		}
+	}
+	.el-info{
+		.el-info-content{
+			padding: 10px 0;
+		}
+		margin-bottom: 10px;
+	}
 </style>
