2ce5f088e47475d011786dd3946d4602d23fa9fc
hytcwangfang <18014746239@163.com>
2017-12-27 15:37:56 +0800

    练习题

--- src/views/paper/exercises/Home.vue
+++ src/views/paper/exercises/Home.vue
@@ -1,24 +1,34 @@
 <template>
-    <el-container>
-        paper/exercise-page
-    </el-container>
+    <el-tabs type="border-card" v-model="activeName">
+        <el-tab-pane label="练习题列表" name="exerciselist">
+            <List></List>
+        </el-tab-pane>
+        <el-tab-pane label="添加练习题" name="exerciseadd">
+            <add-form></add-form>
+        </el-tab-pane>
+    </el-tabs>
 </template>
 
 <script>
+    import List from './List.vue'
+    import Add from './Add.vue'
     export default {
+        components:{
+            List,
+            addForm:Add
+        },
         data() {
             return {
+                activeName:'exerciselist',
             }
         },
-        components: {
-        },
         methods: {
-            handleSelect: {},
+           
         },
         mounted() {
+
         }
     }
-
 </script>
 
 <style scoped lang="scss">
