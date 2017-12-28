cc8e34b9f4b3924f82d69e2b647b86a895a25621
hytcwangfang <18014746239@163.com>
2017-12-28 17:15:06 +0800

    预习题列表详情

--- src/views/preview/example/Detail.vue
+++ src/views/preview/example/Detail.vue
@@ -7,23 +7,78 @@
 			</div>
 		</div>
 		<div class="content">
-			
+			<my-filter :list="filterList" @callback="search"></my-filter>
+			<div class="paper-progress">
+				<table>
+					<tr>
+						<td>已完成比例</td>
+						<td><el-progress :percentage="60" :stroke-width="12"></el-progress></td>
+						<td>预习总体进度</td>
+						<td><el-progress :percentage="60" :stroke-width="12"></el-progress></td>
+					</tr>
+					<tr>
+						<td>未开始预习</td>
+						<td><el-progress :percentage="10" :stroke-width="12"></el-progress></td>
+						<td></td>
+						<td></td>
+					</tr>
+				</table>
+			</div>
 		</div>
 	</div>
 </template>
 <script>
+	import myFilter from '../../common/myFilter.vue'
 	export default{
 		props:{
 			id:{
 				required: true
 			}
 		},
+		components:{
+			myFilter
+		},
 		data(){
 			return {
-
+				filterList:[{
+					title: '院系',
+					field: 'department',
+					children: [{
+						value: 'computer',
+						text: '计算机'
+					},{
+						value: 'accounting',
+						text: '会计'
+					},{
+						value: 'traffic',
+						text: '交运'
+					},{
+						value: 'park',
+						text: '园林'
+					},{
+						value: 'architecture ',
+						text: '建筑与艺术'
+					}]
+				},{
+					title: '班级',
+					field: 'class',
+					children: [{
+						value: '20170801',
+						text: '1班'
+					},{
+						value: '20170802',
+						text: '2班'
+					},{
+						value: '20170803',
+						text: '3班'
+					}]
+				}]
 			}
 		},
 		methods:{
+			search(filterArr){
+
+			},
 			close(){
 				this.$emit('close');
 			},
@@ -32,4 +87,5 @@
 </script>
 <style lang="scss" scoped="">
 	@import '~scss_vars';
+	
 </style>
