87f60dcb977d297c576ed5ba07feb55c2d497c40
hytcwangfang <18014746239@163.com>
2018-01-02 17:25:17 +0800

    考试

---
+++ src/views/exam/info/List.vue
@@ -0,0 +1,152 @@
+<template>
+	<div>
+        <section v-show="!isShowDetail">
+    		<my-filter :list="filterList" @callback="search"></my-filter>
+            <div class="panel">
+                <div class="title">
+                    <el-input placeholder="请输入搜索关键词" v-model="searchkey">
+                        <el-button slot="append" icon="el-icon-search"></el-button>
+                    </el-input>
+                    <div class="pageArea">
+                        <Page :current="currentPage" :total="total" :pageSize="pageSize" @page-change="pageChange"></Page>
+                    </div>
+                    
+                </div>
+                <div class="content">
+                    <el-table :data="papers" highlight-current-row v-loading="listLoading" style="width: 100%;">
+                        <el-table-column type="index" label="序号" width="60">
+                        </el-table-column>
+                        <el-table-column prop="name" label="考试名称" min-width="140">
+                            <template scope="scope">
+                                <el-button type="text" @click="detailShow(scope.row.id)">{{scope.row.name}}</el-button>
+                            </template>
+                        </el-table-column>
+                        <el-table-column prop="paper" label="选择试卷" min-width="140">
+                        </el-table-column>
+                        <el-table-column prop="testRange" label="考试时间" min-width="160">
+                        </el-table-column>
+                        <el-table-column prop="status" label="状态" min-width="100">
+                            <template scope="scope">
+                                <span v-if="scope.row.status==0">未开始</span>
+                                <span v-if="scope.row.status==1">进行中</span>
+                                <span v-if="scope.row.status==2">已结束</span>
+                            </template>
+                        </el-table-column>
+                        <el-table-column prop="listener" label="监测老师" min-width="100">
+                        </el-table-column>
+                        <el-table-column prop="testClass" label="考试人员" min-width="100">
+                        </el-table-column>
+                        <el-table-column label="操作" min-width="60">
+                            <template scope="scope">
+                                <el-button type="primary" v-if="scope.row.status==0">编辑</el-button>
+                            </template>
+                        </el-table-column>
+                    </el-table>
+                </div>
+            </div>
+    	</section>
+        <section v-if="isShowDetail && testId">
+            <test-detail :id="testId" @close="detailClose"></test-detail>
+        </section>
+    </div>
+</template>
+<script>
+	import myFilter from '../../common/myFilter.vue'
+    import Pagination from '../../common/Pagination.vue'
+    import testDetail from './Detail.vue'
+    export default {
+        components:{
+        	myFilter,
+        	Page:Pagination,
+            testDetail
+        },
+        data() {
+            return {
+                searchkey:'',
+                filterList:[{
+                    title:'课程',
+                    field:'project',
+                    children:[{
+                        value:'hysics',
+                        text:'大学物理'
+                    },{
+                        value:'mathematics',
+                        text:'高等数学'
+                    },{
+                        value:'english',
+                        text:'大学英语'
+                    }]
+                },{
+                    title:'状态',
+                    field:'status',
+                    children:[{
+                        value:'0',
+                        text:'未开始'
+                    },{
+                        value:'1',
+                        text:'进行中'
+                    },{
+                        value:'2',
+                        text:'已结束'
+                    }]
+                }],
+                papers: [{
+                    id: '1',
+                    name:'物理测试1',
+                    paper:'物理期中试卷1',
+                    testRange:'2018/01/01 8:00-8:20',
+                    status:'1',
+                    listener:'张老师',
+                    testClass:'16级计算机3班',
+                },{
+                    id: '2',
+                    name:'物理测试2',
+                    paper:'物理期中试卷2',
+                    testRange:'2018/01/01 10:00-10:20',
+                    status:'0',
+                    listener:'张老师',
+                    testClass:'16级计算机3班',
+                },{
+                    id: '3',
+                    name:'物理测试3',
+                    paper:'物理期中试卷2',
+                    testRange:'2018/01/01 10:00-10:20',
+                    status:'2',
+                    listener:'张老师',
+                    testClass:'16级计算机3班',
+                }],
+                total: 10,
+                currentPage: 1,
+                pageSize:5,
+                listLoading: false,
+                isShowDetail: false,
+                testId:''
+            }
+        },
+        methods: {
+            search(value){
+                console.log('search',value);
+            },
+            pageChange(){
+
+            },
+            detailShow(id){
+                this.isShowDetail = true;
+                this.testId = id;
+            },
+            detailClose(){
+                this.isShowDetail = false;
+                this.testId = '';
+            }
+        },
+        mounted() {
+
+        }
+    }
+</script>
+<style scoped lang="scss">
+    @import '~scss_vars';
+    .filter-wrap{
+        margin-bottom: 20px;
+    }
+</style>
---
+++ src/views/exam/publish/Add.vue
@@ -0,0 +1,120 @@
+<template>
+	<section>
+		<div class="panel">
+			<div class="title">
+				<span>发布考试</span>
+				<div class="pull-right">
+					<el-button type="success" @click="onSubmit('form')" class="el-button-shadow">保存</el-button>
+					<el-button type="danger" @click="resetForm('form')" class="el-button-shadow">重置</el-button>
+				</div>
+			</div>
+			<div class="content">
+				<el-form ref="form" :model="form" :rules="rules" label-width="110px" :inline-message="isInlineMessage" @submit.prevent="onSubmit">
+					<el-form-item label="考试名称：" prop="name">
+						<el-input v-model="form.name"></el-input>
+					</el-form-item>
+					<el-form-item label="选择试卷：" prop="test"> 
+						<el-select v-model="form.test" placeholder="请选择试卷">
+							<el-option label="物理测试卷1" value="1"></el-option>
+							<el-option label="物理测试卷2" value="2"></el-option>
+							<el-option label="物理测试卷2" value="3"></el-option>
+						</el-select>
+					</el-form-item>
+					<el-form-item label="考试年级：" prop="grade">
+						<el-select v-model="form.grade" placeholder="请选择考试年级">
+							<el-option label="16级" value="16"></el-option>
+							<el-option label="15级" value="15"></el-option>
+						</el-select>
+					</el-form-item>
+					<el-form-item label="考试院系：" prop="department">
+						<el-select v-model="form.department" placeholder="请选择考试院系">
+							<el-option label="物理工程系" value="wuli"></el-option>
+							<el-option label="计算机工程系" value="jisuanji"></el-option>
+						</el-select>
+					</el-form-item>
+					<el-form-item label="考试班级：" prop="class">
+						<el-select v-model="form.class" placeholder="请选择考试班级">
+							<el-option label="1班" value="1"></el-option>
+							<el-option label="2班" value="2"></el-option>
+						</el-select>
+					</el-form-item>
+					<el-form-item label="监考老师：" prop="listeners">
+						<el-select v-model="form.listeners" placeholder="请选择监考老师">
+							<el-option label="张老师" value="zhang"></el-option>
+							<el-option label="李老师" value="li"></el-option>
+						</el-select>
+					</el-form-item>
+					<el-form-item label="开始时间：" prop="begintime">
+						<el-date-picker type="datetime" placeholder="请选择开始时间" format="yyyy/MM/dd HH:mm" v-model="form.begintime" style="width: 240px;"></el-date-picker>
+					</el-form-item>
+					<el-form-item label="结束时间：" prop="endtime">
+						<el-date-picker type="datetime" placeholder="请选择结束时间" format="yyyy/MM/dd HH:mm" v-model="form.endtime" style="width: 240px;"></el-date-picker>
+					</el-form-item>
+				</el-form>
+			</div>
+		</div>
+	</section>
+</template>
+<script>
+	export default {
+		data() {
+			return {
+				isInlineMessage: true,
+				form: {
+					name: '',
+					test: '',
+					grade: '',
+					department: '',
+					class: '',
+					listeners: '',
+					begintime: '',
+					endtime: ''
+				},
+				rules:{
+					name:[
+						{required: true, message: '请输入考试名称', trigger: 'blur'},
+						{min: 2, max: 30, message: '长度在2-30个字符', trigger: 'blur'}
+					],
+					test:[
+						{required: true, message: '请选择试卷', trigger:'change'}
+					],
+					grade: [
+						{required: true, message: '请选择考试年级', trigger:'change'}
+					],
+					department: [
+						{required: true, message: '请选择考试院系', trigger:'change'}
+					],
+					class: [
+						{required: true, message: '请选择考试班级', trigger:'change'}
+					],
+					listeners:{required:true, message: '请选择监考老师', trigger:'change'},
+					begintime:{required:true, message: '请选择开始时间', trigger:'change'},
+					endtime:{required:true, message: '请选择结束时间', trigger:'change'}
+				}
+			}
+		},
+		computed:{
+			
+		},
+		methods: {
+			onSubmit(formName, flag) {
+				this.$refs[formName].validate((isValid) => {
+					if(isValid){
+						//to do
+						//save ...
+						
+					}else{
+						return false;
+					}
+				});
+			},
+			resetForm(formName){
+				this.$refs[formName].resetFields();
+			},
+		}
+	}
+</script>
+<style scoped lang="scss">
+    @import '~scss_vars';
+    
+</style>
